d2774fe3cf10565013cec544293d952850d5b5a3
abubakar4-u <abubakarsidique694@gmail.com>
2024-05-11 14:27:50 +0500

    Added more inputs to Login, and Update Page.

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import './Login.css';
-import { BsEye, BsEnvelopeFill, BsFillEmojiSmileFill } from "react-icons/bs";
+import { BsEye, BsEnvelopeFill, BsFillEmojiSmileFill, BsFillPersonFill, BsPhone, BsPinMapFill } from "react-icons/bs";
+import { AiOutlinePlus } from "react-icons/ai";
 
 function Login() {
 
@@ -12,24 +13,69 @@ function Login() {
             <div className='login-underline'></div>
         </div>
         <div className='login-inputs'>
-            {action==="Login"?<div></div>:<div className='login-input'>
-              <BsFillEmojiSmileFill/>  
-              <input type="login-text" placeholder='Name'/>
-            </div>}
-            
-            <div className='login-input'>
-              <BsEnvelopeFill/>  
-              <input type="login-email" placeholder='Email'/>
-              
-            </div>
-            <div className='login-input'>
-              <BsEye/>  
-              <input type="login-password" placeholder='Password'/>
-            </div>
+            {action==="Login" ? (
+              <>
+                <div className="login-input">
+                  <BsFillPersonFill/>
+                  <input type="text" placeholder='Username' />
+                </div>
+                <div className="login-input">
+                  <BsEye/>
+                  <input type="password" placeholder='Password'/>
+                </div>
+                <div className='login-input'>
+                  <AiOutlinePlus/>
+                  <select>
+                    <option value="" disabled selected>Select Type</option>
+                    <option value="C">Customer</option>
+                    <option value="A">Admin</option>
+                    <option value="R">Rider</option>
+                  </select>
+                </div>
+              </>
+            ): (
+              <>
+                <div className='login-input'>
+                  <BsFillEmojiSmileFill/>
+                  <input type="text" placeholder='Name'/>
+                </div>
+                <div className='login-input'>
+                  <BsFillPersonFill/>
+                  <input type="text" placeholder='Username'/>
+                </div>
+                <div className='login-input'>
+                  <BsEnvelopeFill/>
+                  <input type="email" placeholder='Email'/>
+                </div>
+                <div className='login-input'>
+                  <BsEye/>
+                  <input type="password" placeholder='Password'/>
+                </div>
+                <div className='login-input'>
+                  <BsPinMapFill/>
+                  <input type="text" placeholder='Address'/>
+                </div>
+                <div className='login-input'>
+                  <BsPhone/>
+                  <input type="text" placeholder="Contact"/>
+                </div>
+                <div className='login-input'>
+                  <AiOutlinePlus/>
+                  <select>
+                    <option value="" disabled selected>Select Type</option>
+                    <option value="C">Customer</option>
+                    <option value="A">Admin</option>
+                    <option value="R">Rider</option>
+                  </select>
+                </div>
+              </>
+            )}
         </div>
-        {action==="Sign Up"?<div></div>:<div className="login-forgot-password">
-            Forgot Password? <span>Click Here!</span>
-        </div>}
+        {action === "Sign Up" ? (
+          <div className="login-forgot-password">
+              Forgot Password? <span>Click Here!</span>
+          </div>
+        ): null}
         
         <div className="login-submit-container">
             <div className={action==="Login"?"login-submit login-gray":"login-submit"} onClick={()=>{setAction("Sign Up")}}>
